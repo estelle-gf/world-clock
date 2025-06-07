81cd4d0ab67126ecda0549b56fa4935adea3ed2e
estelle-gf <estellegfont@gmail.com>
2025-06-07 17:15:21 +0100

    added id's to html elements and basic js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 
+let londonElement=document.querySelector("#london");
+let londonDateElement= londonElement.querySelector(".date");    
+let londonTimeElement= londonElement.querySelector(".time");
+londonDateElement.innerHTML="Monday 1 june 2025";
+londonTimeElement.innerHTML="15:00:01<small>AM</small>";
