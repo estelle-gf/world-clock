7f4732173b600f4ed0aefa1e9d3594c0f9f56326
estelle-gf <estellegfont@gmail.com>
2025-06-07 23:57:22 +0100

    updated js to show correct time

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-
+function updateTime() {
 let londonElement=document.querySelector("#london");
 let londonDateElement= londonElement.querySelector(".date");    
 let londonTimeElement= londonElement.querySelector(".time");
-londonDateElement.innerHTML="Monday 1 june 2025";
-londonTimeElement.innerHTML="15:00:01<small>AM</small>";
+let londonTime =moment().tz("Europe/London");
+londonDateElement.innerHTML= moment().format("MMMM do YYYY");
+londonTimeElement.innerHTML=londonTime.format("h:mm:ss [<small>]A[</small>]");
+
+let parisElement=document.querySelector("#paris");
+let parisDateElement= parisElement.querySelector(".date");    
+let parisTimeElement= parisElement.querySelector(".time");
+let parisTime =moment().tz("Europe/Paris");
+parisDateElement.innerHTML= moment().format("MMMM do YYYY");
+parisTimeElement.innerHTML=londonTime.format("h:mm:ss [<small>]A[</small>]");
+}
+setInterval(updateTime, 1000);
