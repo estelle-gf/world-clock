73163b6bc366131bba394770efd2dc783719bcab
estelle-gf <estellegfont@gmail.com>
2025-06-08 11:21:41 +0100

    added js for current location option

--- script.js
+++ script.js
@@ -3,31 +3,34 @@ let londonElement=document.querySelector("#london");
 let londonDateElement= londonElement.querySelector(".date");    
 let londonTimeElement= londonElement.querySelector(".time");
 let londonTime =moment().tz("Europe/London");
-londonDateElement.innerHTML= moment().format("MMMM do YYYY");
+londonDateElement.innerHTML= moment().format("MMMM Do YYYY");
 londonTimeElement.innerHTML=londonTime.format("h:mm:ss [<small>]A[</small>]");
 
 let parisElement=document.querySelector("#paris");
 let parisDateElement= parisElement.querySelector(".date");    
 let parisTimeElement= parisElement.querySelector(".time");
 let parisTime =moment().tz("Europe/Paris");
-parisDateElement.innerHTML= moment().format("MMMM do YYYY");
+parisDateElement.innerHTML= moment().format("MMMM Do YYYY");
 parisTimeElement.innerHTML=parisTime.format("h:mm:ss [<small>]A[</small>]");
 }
 function updateCity(event) {
     let cityTimeZone = event.target.value;
-    let cityName = city.options[city.selectedIndex].text;
+    if (cityTimeZone === "current") {
+        cityTimeZone = moment.tz.guess();}
+   let cityName = event.target.options[event.target.selectedIndex].text;
+    if (cityName === "Current Location 🌎") {
+        cityName = moment.tz.guess().split("/")[1].replace("_", " ");}
+
     let cityTime = moment().tz(cityTimeZone);
     let citiesElement = document.querySelector("#cities");
     citiesElement.innerHTML =`
     <div class="city">
      <div>
       <h2>${cityName}</h2>
-      
     </div>
     <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
      <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
-  </div>
-`}
+  </div>`} 
 updateTime();
 setInterval(updateTime, 1000);
 
